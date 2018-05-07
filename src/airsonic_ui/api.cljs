94d6c25d9e82bde3aa2d368f8902f2f29f979097
Arne Schlüter <arne@schlueter.is>
2018-05-07 18:15:35 +0200

    Cleanup api interaction; also, not everybody is using my server ;)

--- src/airsonic_ui/api.cljs
+++ src/airsonic_ui/api.cljs
@@ -2,8 +2,8 @@
   (:require [clojure.string :as str]
             [airsonic-ui.config :as config]))
 
-(defn ^:private uri-escape [s]
-  (js/encodeURIComponent s))
+(defn- encode [c]
+  (js/encodeURIComponent c))
 
 (defn url
   "Returns an absolute url to an API endpoint"
@@ -12,21 +12,20 @@
                           :f "json"
                           :c "airsonic-ui-cljs"
                           :v "1.15.0")
-                   (map (fn [[k v]]
-                          (str (uri-escape (name k)) "=" (uri-escape v))))
+                   (map (fn [[k v]] (str (encode (name k)) "=" (encode v))))
                    (str/join "&"))]
-    (str server (when-not (str/ends-with? server "/") "/") "/rest/" endpoint "?" query)))
+    (str server (when-not (str/ends-with? server "/") "/") "rest/" endpoint "?" query)))
 
 (defn song-url [server credentials song]
   (url server "stream" (merge {:id (:id song)} credentials)))
 
-(defn ^:private api-error?
+(defn- api-error?
   "We need to look at the message body because the subsonic api always responds
   with status 200"
   [response]
   (= "failed" (-> response :subsonic-response :status)))
 
-(defn ^:private error-message
+(defn- error-message
   [response]
   (let [{:keys [code message]} (-> response :subsonic-response :error)]
     (str "Code " code ": " message)))
--- src/airsonic_ui/config.cljs
+++ src/airsonic_ui/config.cljs
@@ -1,6 +1,4 @@
 (ns airsonic-ui.config)
 
-(def server "https://londe.arnes.space")
-
 (def debug?
   ^boolean goog.DEBUG)
